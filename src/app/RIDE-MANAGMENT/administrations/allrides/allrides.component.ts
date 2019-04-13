8cd778181c4412a988f43681350db49e9e48bb0f
MuhammadInaamMunir <munni7474@gmail.com>
2019-04-14 00:07:19 +0500

    show all rides and fix some issues on create ride page

--- src/app/RIDE-MANAGMENT/administrations/allrides/allrides.component.ts
+++ src/app/RIDE-MANAGMENT/administrations/allrides/allrides.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppServiceManager } from 'app/services/appServiceManager';
+import {RideModel} from 'app/models/ride.models';
 
 @Component({
     selector: 'app-allrides-cmp',
     templateUrl: 'allrides.component.html'
 })
 
-export class allridesComponent {}
+export class allridesComponent implements OnInit {
+    
+    rides:RideModel[] = [];
+
+    constructor(private appServiceManager: AppServiceManager) { };
+    ngOnInit(){
+        this.getAll();
+    }
+    
+    getAll() {
+        this.appServiceManager.get('rides').subscribe(res => {
+            this.rides = res;
+            console.log(this.rides);
+        });
+    }
+}
--- src/app/RIDE-MANAGMENT/driverpage/creaeteride/creaeteride.component.ts
+++ src/app/RIDE-MANAGMENT/driverpage/creaeteride/creaeteride.component.ts
@@ -1,25 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import {AppServiceManager} from 'app/services/appServiceManager';
-import {RideModel} from 'app/models/ride.models';
+import { AppServiceManager } from 'app/services/appServiceManager';
+import { RideModel } from 'app/models/ride.models';
 
 @Component({
     selector: 'app-creaeteride-cmp',
     templateUrl: 'creaeteride.component.html',
-    styleUrls:['creaeteride.component.css']
+    styleUrls: ['creaeteride.component.css']
 })
 
 export class creaeterideComponent implements OnInit {
     ride: RideModel;
-    constructor(private appServiceManager: AppServiceManager){};
+    wheelChairs = [
+        { id: "true", label: "Yes" },
+        { id: "false", label: "No" }
+    ];
+    rideTypes = [
+        { id: "Normal", label: "Normal" },
+        { id: "Medical", label: "Medical" },
+        { id: "School", label: "School" }
+    ];
+    paymentMethods = [
+        { id: "Cash", label: "Cash" },
+        { id: "Card", label: "Card" }
+    ];
+    constructor(private appServiceManager: AppServiceManager) { };
 
-    ngOnInit(){
-        this.ride = new RideModel(); 
+    ngOnInit() {
+        this.ride = new RideModel();
     }
 
-    createRide(){ 
-            var postData = JSON.stringify(this.ride);
-            this.appServiceManager.post('rides',postData).subscribe(res => {
-                console.log("res", res);
-            });
+    createRide() {
+        var postData = JSON.stringify(this.ride);
+        this.appServiceManager.post('rides', postData).subscribe(res => {
+            console.log("res", res);
+        });
     }
 }
